d042f29317c2a341926618c5b25a738d332e9a52
ChrisLHall <mrchrislhall@gmail.com>
2022-10-01 14:48:23 -0700

    Adding some skeleton files

---
+++ index.js
@@ -0,0 +1,12 @@
+// Gonna put a png of how the site should work in this folder
+
+// Entries will have:
+// Title
+// Chosen option
+// The collection of all of the options
+// Start day when this was decided
+// Length of time that it lasts
+
+// idk if I will keep old entries around or not, doesn't matter yet
+
+
